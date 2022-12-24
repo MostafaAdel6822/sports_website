62854ebe0ca0aa423910d53e9087c4ac09201f99
Zeyad AG <Zeyad AG@ZEYADLAPTOP>
2022-12-24 02:13:20 +0200

    added SportsAssociationManager webform and implemented add match and delete match

---
+++ sports_platform/sports-association-manager.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="sports-association-manager.aspx.cs" Inherits="sports_platform.sports_association_manager" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+ 
+        <div>
+            Sports Association Manager</div>
+        <div>Name of user</div>
+    <form id="form1" runat="server">
+        <p>
+            Add Match</p>
+        <div>
+            <span style="padding:1.4em;">host club name</span> <span style="padding:0.5em;">guest club name</span> <span style="padding-left:2.3em;">start time</span> <span style="padding-left:5em;">end time</span></div>
+        <asp:TextBox ID="host_name_SAM_add" runat="server" OnTextChanged="host_name_SAM_add_TextChanged"></asp:TextBox>
+        <asp:TextBox ID="guest_name_SAM_add" runat="server"></asp:TextBox>
+        <asp:TextBox ID="start_time_SAM_add" runat="server"></asp:TextBox>
+        <asp:TextBox ID="end_time_SAM_add" runat="server"></asp:TextBox>
+        <asp:Button ID="add_match_btn" runat="server" Text="Add" OnClick="add_match_btn_Click" style="width: 80px;margin-left:35px;" />
+    <br>
+    <p>
+        Delete Match</p>
+            <div>
+            <span style="padding:1.4em;">host club name</span> <span style="padding:0.5em;">guest club name</span> <span style="padding-left:2.3em;">start time</span> <span style="padding-left:5em;">end time</span></div>
+        <asp:TextBox ID="host_name_SAM_del" runat="server"></asp:TextBox>
+        <asp:TextBox ID="guest_name_SAM_del" runat="server"></asp:TextBox>
+        <asp:TextBox ID="start_time_SAM_del" runat="server"></asp:TextBox>
+        <asp:TextBox ID="end_time_SAM_del" runat="server"></asp:TextBox>
+        <asp:Button ID="delete_match_btn" runat="server" Text="Delete" OnClick="delete_match_btn_Click" style="width: 80px;margin-left:35px;" />
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ sports_platform/sports-association-manager.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace sports_platform
+{
+    public partial class sports_association_manager : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void add_match_btn_Click(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["Sports_Platform_DB"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            String hostName = host_name_SAM_add.Text;
+            String guestName = guest_name_SAM_add.Text;
+            String startTime = start_time_SAM_add.Text;
+            String endTime = end_time_SAM_add.Text;
+
+            SqlCommand clubs = new SqlCommand("allClubs", conn);
+            clubs.CommandType = CommandType.TableDirect;
+
+            conn.Open();
+            SqlDataReader rdr = clubs.ExecuteReader(CommandBehavior.CloseConnection);
+            bool hostClubFound = false;
+            bool guestClubFound = false;
+            while (rdr.Read())
+            {
+                String clubName = rdr.GetString(rdr.GetOrdinal("name"));
+                if (hostName == clubName)
+                    hostClubFound = true;
+                if (guestName == clubName)
+                    guestClubFound = true;
+            }
+
+            if (hostClubFound && guestClubFound)
+            {
+                SqlCommand addMatch = new SqlCommand("addNewMatch", conn);
+                addMatch.CommandType = CommandType.StoredProcedure;
+
+                addMatch.Parameters.Add(new SqlParameter("@host_name", hostName));
+                addMatch.Parameters.Add(new SqlParameter("@guest_name", guestName));
+                addMatch.Parameters.Add(new SqlParameter("@start_time", startTime));
+                addMatch.Parameters.Add(new SqlParameter("@end_time", endTime));
+
+                conn.Open();
+                addMatch.ExecuteNonQuery();
+                conn.Close();
+            }
+            else
+                Response.Write("Incorrect Club name!");
+
+        }
+
+        protected void delete_match_btn_Click(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["Sports_Platform_DB"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+
+            String hostName = host_name_SAM_del.Text;
+            String guestName = guest_name_SAM_del.Text;
+            // String startTime = start_time_SAM_del.Text;
+            // String endTime = end_time_SAM_del.Text;
+
+            SqlCommand clubs = new SqlCommand("allClubs", conn);
+            clubs.CommandType = CommandType.TableDirect;
+
+            conn.Open();
+            SqlDataReader rdr = clubs.ExecuteReader(CommandBehavior.CloseConnection);
+            bool hostClubFound = false;
+            bool guestClubFound = false;
+            while (rdr.Read())
+            {
+                String clubName = rdr.GetString(rdr.GetOrdinal("name"));
+                if (hostName == clubName)
+                    hostClubFound = true;
+                if (guestName == clubName)
+                    guestClubFound = true;
+            }
+
+            if (hostClubFound && guestClubFound)
+            {
+                SqlCommand deleteMatch = new SqlCommand("deleteMatch", conn);
+                deleteMatch.CommandType = CommandType.StoredProcedure;
+
+                deleteMatch.Parameters.Add(new SqlParameter("@host_name", hostName));
+                deleteMatch.Parameters.Add(new SqlParameter("@guest_name", guestName));
+                //removed 2 parameters for start and end time
+
+                conn.Open();
+                deleteMatch.ExecuteNonQuery();
+                conn.Close();
+            }
+            else
+                Response.Write("Incorrect Club name!");
+        }
+    }
+}
