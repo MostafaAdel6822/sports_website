b5210b210ef79676ef5e8276c5c1de08ff210fe1
MostafaAdel6822 <mostafa.adel6822@gmail.com>
2022-12-28 04:22:42 +0300

    SAM register test

---
+++ sports_platform/SAM_register_page.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace sports_platform
+{
+    public partial class SAM_register_page : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addSAM(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["Sports_Platform_DB"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+            String name = sam_name.Text;
+            String username = sam_username.Text;
+            String password = sam_password.Text;
+            SqlCommand addSAM = new SqlCommand("addAssociationManager", conn);
+            addSAM.CommandType = CommandType.StoredProcedure;
+            addSAM.Parameters.Add(new SqlParameter("@name", name));
+            addSAM.Parameters.Add(new SqlParameter("@username", username));
+            addSAM.Parameters.Add(new SqlParameter("@password", password));
+            conn.Open();
+            addSAM.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Registered Successfully");
+        }
+    }
+}
